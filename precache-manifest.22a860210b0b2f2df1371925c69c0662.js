cb4638621036c28a864259386c91bbc96e680c55
atefgad <atef0777@gmail.com>
2019-03-06 13:05:18 +0200

    Updates

---
+++ precache-manifest.22a860210b0b2f2df1371925c69c0662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a890a3f15edfdf19ca0",
+    "url": "/todo-react/static/js/runtime~main.3a890a3f.js"
+  },
+  {
+    "revision": "bee07135e6ca1bddbe1f",
+    "url": "/todo-react/static/js/main.bee07135.chunk.js"
+  },
+  {
+    "revision": "35a81976d057741a9be4",
+    "url": "/todo-react/static/js/2.35a81976.chunk.js"
+  },
+  {
+    "revision": "bee07135e6ca1bddbe1f",
+    "url": "/todo-react/static/css/main.b4d4d0a3.chunk.css"
+  },
+  {
+    "revision": "c9f23478f02771be520b95dfa8115db6",
+    "url": "/todo-react/index.html"
+  }
+];
